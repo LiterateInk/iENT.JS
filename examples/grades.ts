5cbf8af9840f567249fa89a16f771cdf6d8ca9e0
Mikkel ALMONTE--RINGAUD <contact@vexcited.com>
2025-04-05 14:55:03 +0200

    docs(examples): add grades

---
+++ examples/grades.ts
@@ -0,0 +1,15 @@
+import { credentials } from "./_credentials";
+import * as ient from "../src";
+
+void async function main () {
+  const sessionID = await ient.login(ient.ProfileKind.Student, credentials.username, credentials.password);
+
+  const years = await ient.getGradeYears(sessionID);
+  console.dir(years, { depth: Infinity });
+
+  const currentYear = years.find((year) => year.isDefault);
+  if (!currentYear) throw new Error("current year not found");
+
+  const gradesOverview = await ient.getGradesForYear(sessionID, currentYear.number);
+  console.dir(gradesOverview, { depth: Infinity });
+}();
